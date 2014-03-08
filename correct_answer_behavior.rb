693061bc8321a11c03d7f8310172d9998d6e1121
Mitsutaka Mimura <takkanm@gmail.com>
2014-03-08 16:23:22 +0900

    extract class: Player

--- correct_answer_behavior.rb
+++ correct_answer_behavior.rb
@@ -1,7 +1,14 @@
+class Player
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
 class CorrectAnswerBehavior
   def initialize(seed = nil)
     srand(seed) if seed
-    @players = %w[Alice Bob Cecil]
+    @players = %w[Alice Bob Cecil].map {|name| Player.new(name) }
     @purses = @players.map { rand(3) + 5 }
     @in_penalty_box = @players.map { rand(2) == 0 }
     @current_player = rand(@players.count)
@@ -11,27 +18,27 @@ class CorrectAnswerBehavior
   def was_correctly_answered
     if @in_penalty_box[@current_player]
       if @is_getting_out_of_penalty_box
-        puts "#{current_player} got out of penalty box"
+        puts "#{current_player.name} got out of penalty box"
         puts 'Answer was correct!!!!'
         @purses[@current_player] += 1
-        puts "#{current_player} now has #{@purses[@current_player]} Gold Coins."
+        puts "#{current_player.name} now has #{@purses[@current_player]} Gold Coins."
         winner = did_player_win()
         change_current_player!
-        puts "Player is now #{current_player}"
+        puts "Player is now #{current_player.name}"
         winner
       else
-        puts "#{current_player} stays in penalty box"
+        puts "#{current_player.name} stays in penalty box"
         change_current_player!
-        puts "Player is now #{current_player}"
+        puts "Player is now #{current_player.name}"
         true
       end
     else
       puts "Answer was correct!!!!"
       @purses[@current_player] += 1
-      puts "#{current_player} now has #{@purses[@current_player]} Gold Coins."
+      puts "#{current_player.name} now has #{@purses[@current_player]} Gold Coins."
       winner = did_player_win
       change_current_player!
-      puts "Player is now #{current_player}"
+      puts "Player is now #{current_player.name}"
       return winner
     end
   end
